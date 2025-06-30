dc58b65ad4ad9e6f95a07f4e7f05e5e3fb772cdb
aryan-bhagwat <aryanbhagwat90@gmail.com>
2025-06-30 16:19:55 +0530

    Add script: highest spending customer using SUM and LIMIT

---
+++ highest_spending_customer.sql
@@ -0,0 +1,6 @@
+-- Script: Identify the customer who spent the most overall
+SELECT customer_id, SUM(amount) AS total_spent
+FROM transactions
+GROUP BY customer_id
+ORDER BY total_spent DESC
+LIMIT 1;
