e1511091dd4d82b825cdfc2e30a5b64ce2ea40e6
aryan-bhagwat <aryanbhagwat90@gmail.com>
2025-09-01 22:56:04 +0530

    Add script: Most sold products by quantity

--- most_sold_products.sql
+++ most_sold_products.sql
@@ -1,4 +1,5 @@
 -- Script: Most sold products by quantity
+
 SELECT p.product_id, p.product_name, SUM(o.quantity) AS total_quantity_sold
 FROM order_items o
 JOIN products p ON o.product_id = p.product_id
